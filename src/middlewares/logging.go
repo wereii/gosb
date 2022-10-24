c8e522a51305c827112aaec5d10f731112efeaa5
Tomas <8504482+wereii@users.noreply.github.com>
2022-10-25 01:37:14 +0200

    Fix logging middleware

--- src/middlewares/logging.go
+++ src/middlewares/logging.go
@@ -9,13 +9,13 @@ func LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		logEntry := logrus.WithField("remote", r.RemoteAddr)
 		if r.Header.Get("User-Agent") != "" {
-			logEntry.WithField("User-Agent", r.Header.Get("User-Agent"))
+			logEntry = logEntry.WithField("User-Agent", r.Header.Get("User-Agent"))
 		}
 		if r.Header.Get("X-Forwarded-For") != "" {
-			logEntry.WithField("X-Forwarded-For", r.Header.Get("X-Forwarded-For"))
+			logEntry = logEntry.WithField("X-Forwarded-For", r.Header.Get("X-Forwarded-For"))
 		}
 		if r.Header.Get("X-Real-IP") != "" {
-			logEntry.WithField("X-Real-IP", r.Header.Get("X-Real-IP"))
+			logEntry = logEntry.WithField("X-Real-IP", r.Header.Get("X-Real-IP"))
 		}
 		logEntry.Debugf("Request: %s", r.RequestURI)
 		next.ServeHTTP(w, r)
